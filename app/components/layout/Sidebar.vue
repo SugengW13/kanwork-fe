e020ca243adc412d84953bad9cfa1342588e0e87
sugengw <sugeng.warsito@transtrack.id>
2026-01-11 11:44:47 +0700

    feat: add navigation items

--- app/components/layout/Sidebar.vue
+++ app/components/layout/Sidebar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const { isOpen } = useSidebar()
+const isOpenCollapsible = ref(false)
 </script>
 
 <template>
@@ -12,10 +13,59 @@ const { isOpen } = useSidebar()
     @click.self="isOpen = false"
   >
     <div
-      class="absolute bg-white top-0 bottom-0 w-100 border-r border-accented space-y-4 px-5 py-10 transition-transform duration-300"
+      class="absolute bg-white top-0 bottom-0 w-100 border-r border-accented space-y-4 flex flex-col px-5 py-10 transition-transform duration-300"
       :class="{ '-translate-x-full': !isOpen }"
     >
-      sidebar
+      <nuxt-link to="#">
+        <div class="hover:bg-primary/5 rounded-lg px-5 py-3 space-x-2 transition flex items-center">
+          <u-icon
+            name="material-symbols:bar-chart-rounded"
+            size="20"
+          />
+
+          <p class="text-lg font-medium">Reporting</p>
+        </div>
+      </nuxt-link>
+
+      <u-collapsible
+        v-model:open="isOpenCollapsible"
+        class="flex flex-col space-y-2 select-none"
+      >
+        <div class="hover:bg-primary/5 rounded-lg px-5 py-3 transition flex items-center justify-between cursor-pointer">
+          <div class="space-x-2 flex items-center">
+            <u-icon
+              name="material-symbols:groups-outline-rounded"
+              size="20"
+            />
+
+            <p class="text-lg font-medium">
+              Teams
+            </p>
+          </div>
+
+          <u-icon
+            name="material-symbols:keyboard-arrow-down-rounded"
+            size="20"
+            class="transition-transform duration-300"
+            :class="{ 'rotate-180': isOpenCollapsible }"
+          />
+        </div>
+
+        <template #content>
+          <div class="space-y-3 flex flex-col">
+            <nuxt-link
+              v-for="i in 5"
+              :key="`team-${i}`"
+              to="#"
+            >
+              <div class="hover:bg-primary/5 rounded-lg px-5 py-3 space-x-2 transition flex items-center">
+                <div class="w-5" />
+                <p class="text-lg font-medium">Team {{ i }}</p>
+              </div>
+            </nuxt-link>
+          </div>
+        </template>
+      </u-collapsible>
     </div>
   </div>
 </template>
